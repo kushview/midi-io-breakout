77cea9f110cd66ad3e800bd1c80bf3967629bb26
Michael Fisher <mfisher@kushview.net>
2020-03-04 18:19:47 -0500

    Exclude gerbers from git

--- tools/gerbers.sh
+++ tools/gerbers.sh
@@ -2,6 +2,6 @@
 name="MIDIBRK_gerbers"
 mkdir -p build/
 rm -rf "build/${name}"
-rsync -var --update "midi_io/gerbers/" "build/${name}"
+rsync -var --update --exclude=".keep" "midi_io/gerbers/" "build/${name}"
 rm -f "build/${name}.zip"
 cd build && zip -vrT "${name}.zip" "${name}" && rm -rf "${name}"
